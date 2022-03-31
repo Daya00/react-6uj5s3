7836de00c9c9c31d0a152fbca5743842593d3bcf
Daya00 <dayanandsingh.it@gmail.com>
2022-03-31 17:11:39 +0530

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 import User from './components/User';
-import ErrorBownd from './components/ErrorBownd';
+import ErrorBownd from './components/ErrorBoundary';
 
 export default function App() {
   return (
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-
 export default function User() {
-  if (true) {
-    throw new Error('Custom Error0');
-  }
   return (
     <div>
       <h1>Hello Daya User!</h1>
